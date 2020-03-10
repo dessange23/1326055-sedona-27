13ca9f1514b6f3bc7cee653f19c3d85e268cb57e
dessange23 <60140019+dessange23@users.noreply.github.com>
2020-03-10 23:38:07 +0500

    финал

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+
 const searchBtn = document.querySelector('.sedona-search');
 const searchForm = document.querySelector('.search-form-container');
 
 searchBtn.addEventListener('click', function addListener(e) {
 	searchForm.classList.toggle('hidden');
-})
+}) 
