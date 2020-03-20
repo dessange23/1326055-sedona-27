3d7c3a4351ba6a99299075494e2b0da1dfe485b9
dessange23 <60140019+dessange23@users.noreply.github.com>
2020-03-20 18:02:19 +0500

    j

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 const searchBtn = document.querySelector('.sedona-search');
 const searchForm = document.querySelector('.search-form-container');
+const inputIn = document.querySelector('.input-1');
+const inputOut = document.querySelector('.input-2');
+const submitBtn = document.querySelector('.found-sedona-hotels');
+const inputAdult = document.querySelector('.adult');
+const inputChild = document.querySelector('.child');
 
-searchBtn.addEventListener('click', function addListener(e) {
+searchBtn.addEventListener('click', () => {
 	searchForm.classList.toggle('hidden');
-})
+});
+
+submitBtn.addEventListener('click', (e) =>  {	
+   if (inputIn.value === '' || inputOut.value === '' || (inputAdult.value === '' && inputChild.value === '')) {
+	  e.preventDefault();
+	  searchForm.style.animation = 'shake 1s ease';
+	  searchForm.style.animationFillMode = 'forwards';
+	  setTimeout(() => {
+		searchForm.style.animation = '';
+		searchForm.style.animationFillMode = '';
+	   }, 1000);
+   }
+});
